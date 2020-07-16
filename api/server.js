f76e2a3c8abd9324d79af37dc4df3300552dab83
Jacob Dykstra <JDykstra@redwoodcu.org>
2020-07-15 20:21:16 -0700

    navbar added

--- api/server.js
+++ api/server.js
@@ -1,28 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const routes = require("./routes");
+// const routes = require("./routes");
 const cors = require('cors');
 
 const app = express();
 const { NODE_ENV } = process.env;
-let PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
+
+const PORT = NODE_ENV === "production" ? process.env.port : 5023;
+// app.use('/api', routes);
 
 if (NODE_ENV === "production" || NODE_ENV === "test") {
-  PORT = NODE_ENV === "production" ? process.env.PORT : 5000;
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
-} else {
-  PORT = 5000;
-}
+} 
 
 // Starts the server to begin listening
 // =============================================================
